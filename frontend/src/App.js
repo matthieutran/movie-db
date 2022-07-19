215eadd4ad4c5e5915873c9118868dd7a963c383
Matthieu Tran <matthieuktran@gmail.com>
2022-07-19 15:16:31 -0700

    Add routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,60 @@
-import "./App.css";
+import React, { Fragment } from "react";
+import { HashRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-function App() {
-  return <div className="App"></div>;
+export default function App() {
+  return (
+    <Router>
+      <div className="container">
+        <div className="row">
+          <h1 className="mt-3">Movie Database</h1>
+        </div>
+
+        <hr className="mb-3" />
+
+        <div className="row">
+          <div className="col-md-2">
+            <nav>
+              <ul className="list-group">
+                <li className="list-group-item">
+                  <Link to="/">Home</Link>
+                </li>
+                <li className="list-group-item">
+                  <Link to="/movies">Movies</Link>
+                </li>
+                <li className="list-group-item">
+                  <Link to="/admin">Manage Catalogue</Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
+
+          <div className="col-md-10">
+            <Switch>
+              <Route path="/movies">
+                <Movies />
+              </Route>
+              <Route path="/admin">
+                <Admin />
+              </Route>
+              <Route path="/">
+                <Home />
+              </Route>
+            </Switch>
+          </div>
+        </div>
+      </div>
+    </Router>
+  );
 }
 
-export default App;
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function Movies() {
+  return <h2>Movies</h2>;
+}
+
+function Admin() {
+  return <h2>Manage Catalog</h2>;
+}
